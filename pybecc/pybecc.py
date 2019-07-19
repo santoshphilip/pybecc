cfde03dac160be7a742de5e65c83222662fe0583
santoshphilip <santosh_philip@yahoo.com>
2019-07-19 10:13:38 -0700

    further development

--- pybecc/pybecc.py
+++ pybecc/pybecc.py
@@ -1,3 +1,57 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import xml.etree.ElementTree as ET
+from collections import OrderedDict
+
+def getroottag(tree):
+    """get the root of the document"""
+    root = tree.getroot()
+    return root.tag
+    
+def getlevel1_tags(root):
+    """get all tags for level below root without dups - maintain order"""
+    childs = [child.tag for child in root]    
+    # prunechilds = set(childs)
+    # cant use set, since I want to preserve order
+    l1tags = list(OrderedDict.fromkeys(childs))
+    return l1tags
+    
+def getchildtags(element):
+    """get tags at child level without dups - maintain order"""
+    childs = [child.tag for child in element]    
+    # prunechilds = set(childs)
+    # cant use set, since I want to preserve order
+    childtags = list(OrderedDict.fromkeys(childs))
+    return childtags
+    
+def getallchildren(element):
+    """get all the children of the element"""
+    return [child for child in element]    
+
+def findalltag_children(element, tag):
+    """find all children that have this tag"""
+    return element.findall(tag)
+    
+def get_tags_childrens_tags(element, tag):
+    """get the tags of all the children that have this tag with no dups
+    - retain the order"""
+    # findalltag_children
+    # for each in that:
+    #     list.append its child tags
+    # remove dups
+    # return
+    c_elements = findalltag_children(element, tag)
+    alist = []
+    for c_element in c_elements:
+        for child in c_element:
+            alist.append(child.tag)
+    tags = list(OrderedDict.fromkeys(alist))
+    return tags
+    
+        
+        
+    
+# get_tags_childrens_tags -> get the tags of all the children that have this tag
+# findalltag_children -> find all childred that have this tag
--- tests/test_pybecc.py
+++ tests/test_pybecc.py
@@ -4,22 +4,146 @@
 """Tests for `pybecc` package."""
 
 import pytest
-
-
+import xml.etree.ElementTree as ET
+from io import StringIO
 from pybecc import pybecc
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+def test_getroot():
+    """py.test for getroot"""
+    data = ((Tree().treetxt1, "data"),  # treetxt, expected
+    )
+    for treetxt, expected in data:
+        tree = ET.ElementTree(ET.fromstring(treetxt))
+        result = pybecc.getroottag(tree)
+        assert result == expected
+        
+def test_getlevel1_tags():
+    """py.test for getlevel1_tags"""
+    data = (
+        (Tree().treetxt1, ['country']),  # treetxt, expected
+        (Tree().treetxt2, ['country', 'city']),  # treetxt, expected
+    )
+    for treetxt, expected in data:
+        tree = ET.ElementTree(ET.fromstring(treetxt))
+        root = tree.getroot()
+        result = pybecc.getlevel1_tags(root)
+        assert result == expected
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_getchildtags():
+    """py.test for getchildtags"""
+    data = ((Tree().treetxt1, ['rank', 'year', 'gdppc', 'neighbor']),  # treetxt, expected
+    )
+    for treetxt, expected in data:
+        tree = ET.ElementTree(ET.fromstring(treetxt))
+        root = tree.getroot()
+        element = root.find('country')
+        print(element)
+        result = pybecc.getchildtags(element)
+        assert result == expected
+        
+def test_getallchildren():
+    """py.test for getallchildren"""
+    data = (
+    (Tree().treetxt1, ['country', 'country', 'country']),  # treetxt, expected
+    )        
+    for treetxt, expected in data:
+        tree = ET.ElementTree(ET.fromstring(treetxt))
+        element = tree.getroot()
+        result = pybecc.getallchildren(element)
+        assert [item.tag for item in result] == expected
+        
 
+def test_findalltag_children():
+    """py.test for findalltag_children"""
+    data = (
+    (Tree().treetxt1, 'country', 
+    ['country', 'country', 'country']),  
+    # treetxt, tag, expected
+    (Tree().treetxt2, 'country', 
+    ['country', 'country', 'country']),  
+    # treetxt, tag, expected
+    )
+    for treetxt, tag, expected in data:
+        tree = ET.ElementTree(ET.fromstring(treetxt))
+        root = tree.getroot()
+        element = root
+        result = pybecc.findalltag_children(element, tag)
+        assert [item.tag for item in result] == expected
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_get_tags_childrens_tags():
+    """py.test for get_tags_childrens_tags"""
+    data = (
+    (Tree().treetxt1, 'country', 
+    ['rank', 'year', 'gdppc', 'neighbor', 'capital']),  
+    # treetxt, tag, expected
+    (Tree().treetxt2, 'country', 
+    ['rank', 'year', 'gdppc', 'neighbor', 'capital']),  
+    # treetxt, tag, expected
+    )
+    for treetxt, tag, expected in data:
+        tree = ET.ElementTree(ET.fromstring(treetxt))
+        root = tree.getroot()
+        element = root
+        result = pybecc.get_tags_childrens_tags(element, tag)
+        assert result == expected
+    
+class Tree(object):
+    """holds tree fata for testing"""
+    def __init__(self):
+        self.treetxt1 = """<?xml version="1.0"?>
+<data>
+    <country name="Liechtenstein">
+        <rank>1</rank>
+        <year>2008</year>
+        <gdppc>141100</gdppc>
+        <neighbor name="Austria" direction="E"/>
+        <neighbor name="Switzerland" direction="W"/>
+    </country>
+    <country name="Singapore">
+        <rank>4</rank>
+        <year>2011</year>
+        <gdppc>59900</gdppc>
+        <neighbor name="Malaysia" direction="N"/>
+    </country>
+    <country name="Panama">
+        <rank>68</rank>
+        <year>2011</year>
+        <gdppc>13600</gdppc>
+        <neighbor name="Costa Rica" direction="W"/>
+        <neighbor name="Colombia" direction="E"/>
+        <capital>Panama City</capital>
+    </country>
+</data>"""
+        # -----------------------
+        self.treetxt2 = """<?xml version="1.0"?>
+<data>
+    <country name="Liechtenstein">
+        <rank>1</rank>
+        <year>2008</year>
+        <gdppc>141100</gdppc>
+        <neighbor name="Austria" direction="E"/>
+        <neighbor name="Switzerland" direction="W"/>
+    </country>
+    <country name="Singapore">
+        <rank>4</rank>
+        <year>2011</year>
+        <gdppc>59900</gdppc>
+        <neighbor name="Malaysia" direction="N"/>
+    </country>
+    <country name="Panama">
+        <rank>68</rank>
+        <year>2011</year>
+        <gdppc>13600</gdppc>
+        <capital>Panama City</capital>
+        <neighbor name="Costa Rica" direction="W"/>
+        <neighbor name="Colombia" direction="E"/>
+    </country>
+    <city name="Chennai">
+        <rank>681</rank>
+        <year>2011</year>
+        <gdppc>13600</gdppc>
+        <neighbor name="Kochi" direction="W"/>
+        <neighbor name="Madurai" direction="S"/>
+    </city>
+</data>"""    
