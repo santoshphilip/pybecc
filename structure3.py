9c713d3ea130c07e7b3979b0690cf1ece7f1d0c3
santoshphilip <santosh_philip@yahoo.com>
2019-09-01 16:25:07 -0700

    clean up

--- structure3.py
+++ structure3.py
@@ -5,6 +5,7 @@ from pybecc import cbecc_structure
 
 fname1 = "./resources/010012-SchSml-CECStd.xml"
 fname1 = "./resources/tree2.xml"
+fname1 = "/Users/santoshphilip/Dropbox/coolshadow_dropbox/doe2eppystuff/eplusfiles/190729_T24_05_mech_process12_upprocess4.cibd16x"
 # fname1 = "/Users/santosh/Dropbox/temp/190715_T24_00-post_open.xml"
 # fname1 = "/Users/santoshphilip/Dropbox/temp/190715_T24_00-post_open.xml"
 # fname1 = "/Users/santosh/Dropbox/temp/scratch/workingfile.cibd16x"
@@ -20,4 +21,4 @@ res = list()
 # res = getchildtags3(xpath, res, fromtag='Proj', uptoindent=None)
 # res = getchildtags3(root, xpath, res, fromtag=None, uptoindent=None, thefile=None)
 res = cbecc_structure.structure(root)
-res = cbecc_structure.structurepath(root)
+# res = cbecc_structure.structurepath(root)
