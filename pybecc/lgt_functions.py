c7e1953db832621dad37b683c975f18b1d79acd8
santoshphilip <santosh_philip@yahoo.com>
2019-09-19 08:29:21 -0700

    modified:   pybecc/lgt_functions.py

--- pybecc/lgt_functions.py
+++ pybecc/lgt_functions.py
@@ -58,19 +58,6 @@ def splitlist(lst, chunk):
     return splits
 
 
-def test_splitlist():
-    """py.test for splitlist"""
-    data = (
-        ([1, 2, 3], 5, [[1, 2, 3]]),  # lst, chunk, expected
-        ([1, 2, 3], 2, [[1, 2], [3]]),  # lst, chunk, expected
-        ([1, 2, 3, 4], 2, [[1, 2], [3, 4]]),  # lst, chunk, expected
-        ([1, 2, 3], 3, [[1, 2, 3]]),  # lst, chunk, expected
-        ([], 5, [[]]),  # lst, chunk, expected
-    )
-    for lst, chunk, expected in data:
-        result = splitlist(lst, chunk)
-        assert result == expected
-
 
 def space_lightrows(rows, headers=2):
     """convert rows to dict {spacename: lightrows, }
