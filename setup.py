53d1263fd7fe9025a74ed3fbbb4469a076b05e5a
santoshphilip <santosh_philip@yahoo.com>
2019-09-26 08:22:55 -0700

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pyenergyplus/pybecc",
-    version="version='0.1.8'",
+    version="version='0.1.9'",
     zip_safe=False,
 )
